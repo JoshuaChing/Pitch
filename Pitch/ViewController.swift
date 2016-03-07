4295bba00aa01d0d306486571b9f3e4707a7fd31
Joshua Ching <jchingdev@gmail.com>
2016-03-06 23:41:12 -0500

    start record, stop record label logic

--- Pitch/ViewController.swift
+++ Pitch/ViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,9 +27,15 @@ class ViewController: UIViewController {
 
     @IBAction func startRecording(sender: AnyObject) {
         recordingLabel.text = "Recording in Progress"
+        recordButton.enabled = false
+        stopRecordButton.enabled = true
     }
 
     @IBAction func stopRecording(sender: AnyObject) {
+        recordingLabel.text = "Tap to Record"
+        recordButton.enabled = true
+        stopRecordButton.enabled = false
     }
+
 }
 
