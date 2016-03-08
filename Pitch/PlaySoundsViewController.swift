7d5ff8ad55b127405f95f5cd0ab4e8410f4cff2a
Joshua Ching <jchingdev@gmail.com>
2016-03-07 23:14:33 -0500

    another view controller for play sounds

---
+++ Pitch/PlaySoundsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch
+//
+//  Created by Joshua Ching on 2016-03-07.
+//  Copyright © 2016 Joshua Ching. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
