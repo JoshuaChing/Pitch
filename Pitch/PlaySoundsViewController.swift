abc40f84e21dd5d879067ca01867e5c5fdb69258
Joshua Ching <jchingdev@gmail.com>
2016-03-08 23:39:59 -0500

    change rate play

--- Pitch/PlaySoundsViewController.swift
+++ Pitch/PlaySoundsViewController.swift
@@ -19,16 +19,20 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
-    var recordedAudio: NSURL!
-    var audioFile: AVAudioFile!
-    var audioEngine: AVAudioEngine!
-    var audioPlayerNode: AVAudioPlayerNode!
-    var stopTimer: NSTimer!
+    let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+    let recordingName = "pitchRecording.wav"
+    
+    var audioPlayer = AVAudioPlayer()
     
     enum ButtonType: Int {case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb}
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let filePath = NSURL.fileURLWithPathComponents([dirPath, recordingName])
+        try! audioPlayer = AVAudioPlayer(contentsOfURL: filePath!, fileTypeHint:nil)
+        audioPlayer.enableRate = true
+        audioPlayer.prepareToPlay()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,18 +40,26 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSoundForButton(sender: UIButton) {
+
+        audioPlayer.currentTime = 0
+        audioPlayer.rate = 2.0
+        
         switch(ButtonType(rawValue: sender.tag)!) {
         case .Slow:
             print("slow")
+            audioPlayer.rate = 0.5
             break
         case .Fast:
             print("fast")
+            audioPlayer.rate = 2.0
             break
         case .Chipmunk:
             print("chipmunk")
+            audioPlayer.rate = 1.0
             break
         case .Vader:
             print("vader")
+            audioPlayer.rate = 1.0
             break
         case .Echo:
             print("echo")
@@ -57,10 +69,12 @@ class PlaySoundsViewController: UIViewController {
             break
         }
         
+        audioPlayer.play()
+        
     }
     
     @IBAction func stopButtonPressed(sender: AnyObject) {
-        
+        audioPlayer.stop()
     }
 
 }
